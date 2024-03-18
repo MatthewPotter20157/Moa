54b1ccb81cff696f4babdc20f06c6fdf4ae23dd3
20157 <20157@wc.school.nz>
2024-03-19 10:46:28 +1300

    test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
 
-DATABASE = "C:/Users/20157/OneDrive - Wellington College/13 DT/Project/Dicitionary.db"
+DATABASE = "C:/Users/20157/OneDrive - Wellington College/13 DT/Project/Dictionary.db"
 
 
 def create_connection(db_file):
@@ -22,8 +22,9 @@ def render_homepage():
 
 
 @app.route('/menu')
-def render_homepage():
-    query = "SELECT Maori, English, Category, Definition, Level FROM Words"
+def menu():
+    con = create_connection(DATABASE)
+    query = "SELECT Maori, English, Category, Level, Definition FROM Words"
     cur = con.cursor()
     cur.execute(query)
     word_list = cur.fetchall()
@@ -32,34 +33,9 @@ def render_homepage():
     return render_template('menu.html', words=word_list)
 
 
-@app.route('/signup', methods=['POST', 'GET'])
-def render_signup_page():
-    if request.method('POST'):
-        print(request.form)
-        fname = request.form.get('fname').title().strip()
-        lname = request.form.get('lname').title().strip()
-        email = request.form.get('email').lower().strip()
-        password = request.form.get('password')
-        password2 = request.form.get('password2')
-
-        if password != password2:
-            return redirect("/signup?error=Passwords+do+not+match")
-
-        if password < 8:
-            return redirect("/signup?error=Password+must+be+at+least+8+letters")
-
-        con = open_database('DATABASE')
-        query = 'INSERT INTO user (fname, lname, email, password) VALUES (?, ?, ?, ?)'
-
-        try:
-            cur.execute(query, (fname, lname, email, password))
-        except sqlite3.IntegrityError:
-            con.close()
-            return redirect("/signup?error=Email+is+all+ready+in+use")
-    return render_template('signup.html')
-
-
-
+@app.route('/login', methods=['POST', 'GET'])
+def render_login_page():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
